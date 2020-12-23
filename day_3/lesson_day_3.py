dbf90420f371b7c016c57f46eff7921285a1d48c
Richard Farr <RichardFarr7@gmail.com>
2020-12-23 11:52:19 -0800

    day 3 lunchtime

---
+++ day_3/lesson_day_3.py
@@ -0,0 +1,16 @@
+# 101 - Day 3
+# RF
+
+fruits = ["fruit 1", "fruit 2", "fruit 3"]
+
+# For Loops
+# They are code loops that repeat
+
+# Method 1: for item in sequence
+# for fruit in fruits:
+#     print(fruit) # or any number of lines of code here
+
+
+# Method 2: for x in range()
+for x in range(1, 12, 2):
+    print(x)
